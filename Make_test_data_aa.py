17460d632d772f9f09c2810f095da68ab1459cd4
Petbys <92719116+Petbys@users.noreply.github.com>
2023-11-29 11:26:17 +0100

    update

--- Make_test_data_aa.py
+++ Make_test_data_aa.py
@@ -3,16 +3,9 @@ import argparse
 import os
 import sys
 
-def create_list_headers(headers):
-    header_list = []
-    with open(headers,"r") as headers:
-        for header in headers:
-            header_list.append(header.rstrip("\n"))
-    return header_list
 '''
-def create_fasta_from_headers(header_list, input_fasta_file, output_fasta_file):
+def create_fasta_from_headers(input_fasta_file, output_fasta_file):
     sequences_to_write = []
-
     # Loop through the input FASTA file and extract sequences based on headers
     for record in SeqIO.parse(input_fasta_file, "fasta"):
         if record.id in header_list:
@@ -26,12 +19,22 @@ def create_fasta_from_headers(header_list, input_fasta_file, output_fasta_file):
     with open(output_fasta_file, "w") as output_handle:
         SeqIO.write(sequences_to_write, output_handle, "fasta")
 '''
+def copy_fasta_with_10_positions(input_fasta, output_fasta):
+    with open(input_fasta, 'r') as f_in, open(output_fasta, 'w') as f_out:
+        header = None
+        for line in f_in:
+            if line.startswith('>'):
+                header = line.rstrip()
+                f_out.write(f"{header}\n")
+            else:
+                sequence_10_positions = line[:10]
+                f_out.write(f"{sequence_10_positions}\n")
+'''
 def extract_sequences(headers, fasta_file, output_file):
     extracted_sequences = {}
     current_header = None
     with open(fasta_file, 'r') as fasta_file:
         for line in fasta_file:
-            print(line)
             line = line.strip()
             if line.startswith('>'):
                 current_header = line
@@ -44,7 +47,7 @@ def extract_sequences(headers, fasta_file, output_file):
         for header, sequence in extracted_sequences.items():
             sequence_10_positions = sequence[:10]
             f_out.write(f">{header}\n{sequence_10_positions}\n")
-
+'''
 if __name__ == '__main__':
         parser = argparse.ArgumentParser(
             description="testing pyasr")
@@ -52,10 +55,12 @@ if __name__ == '__main__':
             "--fasta",
             required=True,
         )
+        '''
         parser.add_argument(
             "--headers",
             required=True,
         )
+        '''
         parser.add_argument(
             "--output",
             required=True,
@@ -63,9 +68,9 @@ if __name__ == '__main__':
 
         args = parser.parse_args()
         fasta_path= args.fasta
-        headers_path = args.headers
         output_path = args.output
 
-        header_list = create_list_headers(headers_path)
-        print(header_list)
-        extract_sequences(header_list, fasta_path, output_path)
+        #header_list = create_list_headers(headers_path)
+        #print(header_list)
+        #extract_sequences(header_list, fasta_path, output_path)
+        copy_fasta_with_10_positions(fasta_path,output_path)
